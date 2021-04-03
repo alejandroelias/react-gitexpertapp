abe126e5aece32c8c95e64dfc13319289f38ba31
Alejandro Elias <alfredo.elias.o@gmail.com>
2021-04-03 11:26:26 -0600

    First commint

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GigGrid } from './components/GigGrid';
+
+export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
+    const [categories, setCategories] = useState( ['One Punch']);
+    
+    // const handleAdd = () =>{
+
+    //     // setCategories([...categories, 'Dark']) 
+    //     setCategories( cats => [...categories, 'Dark']);
+        
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories = { setCategories }/>
+            <hr />
+
+            <ol>
+            {
+                categories.map( category => (
+                    <GigGrid
+                        key = { category }
+                        category = { category }
+                    />
+                ))
+            }
+            </ol>
+        </>
+    )
+}
